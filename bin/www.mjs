f5f5ee5e2455370d6dbbf8fbdf0f3a213a2b4bf6
Abhay <80220229+EpicGamer007@users.noreply.github.com>
2022-11-14 00:50:13 +0000

    sigh

--- bin/www.mjs
+++ bin/www.mjs
@@ -19,19 +19,3 @@ server.listen(PORT, () => {
 		NODE_ENV
 	);
 });
-
-setInterval(() => {
-	process.stdout.write("Ping... ")
-	fetch(`https://pingpong.epicgamer007.repl.co/ping?url=https://epicgamer007.repl.co&time=${Date.now()}`, {
-		cache: "no-store",
-		headers: {
-			"pragma": "no-cache",
-			"cache-control": "no-cache",
-			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-			"Accept-Encoding": "gzip, deflate, br",
-			"Accept-Language": "en-US,en;q=0.5",
-			"Upgrade-Insecure-Requests": "1",
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:106.0) Gecko/20100101 Firefox/106.0"
-		}
-	}).then(() => process.stdout.write("Pong\n"));
-}, 5 * 60 * 1000);
